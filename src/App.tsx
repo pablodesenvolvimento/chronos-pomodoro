dd9d839b342cbe0a220809e59f5ade1baa051c0d
Pablo Lima Braga <pablolbraga@gmail.com>
2025-09-08 20:47:18 -0300

    Criando a estrutura HTML e CSS do app para cada seção do site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading'
+
 
 export function App() {
     return <>
-        <Heading />
-    </>
+        <div className='container'>
+            <div className="content">
+                <section>LOGO</section>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className="content">
+                <section>MENU</section>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className="content">
+                <section>FORM</section>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className="content">
+                <section>FOOTER</section>
+            </div>
+        </div>
+    </>;
 }
